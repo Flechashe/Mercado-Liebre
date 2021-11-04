a15b3f51f7d29d786f9f198c716bf595a15fcce4
Flechashe <se.amigos@gmail.com>
2021-11-04 20:00:47 -0300

    Added Procfile and dynamic port.

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express();
 const publicPath = path.resolve(__dirname, 'public');
 
 app.use(express.static(publicPath));
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log('Server running on port 3000.');
 });
 
